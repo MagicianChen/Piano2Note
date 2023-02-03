18938fd8aa9fdfa4c1d626f3304e6eb56cca8b65
MagicianChen <q1218605102@gmail.com>
2023-02-03 16:01:09 +0800

    updated

--- music.py
+++ music.py
@@ -106,17 +106,17 @@ class pianoTrans():
         :param results:
         :return:
         '''
-        index = len(results)  # 获取需要写入数据的行数
-        workbook = xlrd.open_workbook(os.path.join(save_path, file_name))  # 打开工作簿
-        sheets = workbook.sheet_names()  # 获取工作簿中的所有表格
-        worksheet = workbook.sheet_by_name(sheets[0])  # 获取工作簿中所有表格中的的第一个表格
-        rows_old = worksheet.nrows  # 获取表格中已存在的数据的行数
-        new_workbook = xlutils.copy.copy(workbook)  # 将xlrd对象拷贝转化为xlwt对象
-        new_worksheet = new_workbook.get_sheet(0)  # 获取转化后工作簿中的第一个表格
+        index = len(results)  
+        workbook = xlrd.open_workbook(os.path.join(save_path, file_name))
+        sheets = workbook.sheet_names()
+        worksheet = workbook.sheet_by_name(sheets[0])
+        rows_old = worksheet.nrows
+        new_workbook = xlutils.copy.copy(workbook)
+        new_worksheet = new_workbook.get_sheet(0)
         for i in range(0, index):
             for j in range(0, len(results[i])):
-                new_worksheet.write(i + rows_old, j, results[i][j])  # 追加写入数据，注意是从i+rows_old行开始写入
-        new_workbook.save(os.path.join(save_path, file_name))  # 保存工作簿
+                new_worksheet.write(i + rows_old, j, results[i][j])
+        new_workbook.save(os.path.join(save_path, file_name))
         print("{} 实验结果写入完成！".format(file_name.split('.')[0]))
 
 
